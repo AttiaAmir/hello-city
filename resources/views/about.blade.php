12d17455dd33ac745db0f9b50e8eab5ac193982a
Amir <amirattia97@gmail.com>
2021-09-05 10:56:16 +0100

    Add about page and update links

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,14 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello City</title>
+        <title>About us | Hello City</title>
 
         
     </head>
     <body >
-        <h1>Hello from Tunisia </h1>
-
-        <p>it's currently {{date('h:i A')}}.</p>
+        <p>Built with &hearts; by Attia Amir</p>
+        <p><a href="/">Revenir à la page d'accueil</a></p>
 
         <footer>
     <p>&copy; Copyright {{date('Y')}} &middot; <a href="/about-us">About us</a></p>        
