a6e5660c516c549262bd87f3e9f192177cd9476a
Amir <amirattia97@gmail.com>
2021-09-06 15:25:52 +0100

    Remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
+@section('content')
 
-        <title>About us | Hello City</title>
-
-        
-    </head>
-    <body >
         <p>Built with &hearts; by Attia Amir</p>
         <p><a href="/">Revenir à la page d'accueil</a></p>
 
-        <footer>
-    <p>&copy; Copyright {{date('Y')}} </p>        
-    </footer>
+        
              
-    </body>
-</html>
+  @endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,21 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
+@section('content')
 
-        <title>Hello City</title>
-
-        
-    </head>
-    <body >
         <h1>Hello from Tunisia </h1>
 
         <p>it's currently {{date('h:i A')}}.</p>
 
-        <footer>
-    <p>&copy; Copyright {{date('Y')}} &middot; <a href="/about-us">About us</a></p>        
-    </footer>
+        
              
-    </body>
-</html>
+    @endsection
